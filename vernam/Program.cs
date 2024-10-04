e00f35ef5ba8b16e54577c4d24064679cc18bdf9
Yehor Radchenko <233542@esher.ac.uk>
2024-10-04 13:17:16 +0100

    Add project files.

---
+++ vernam/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vernam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args.Count) 
+            {
+                case (2):
+                    try
+                    {
+
+                    }
+                    catch 
+                    {
+                    
+                    }
+                    break;
+                default:
+                    Help();
+                    Environment.ExitCode(0);
+            }
+        }
+
+        private void Help()
+        {
+            Console.WriteLine("Usage:\n    vernam [CIPHER_TEXT] [PATH/TO/KEY]");
+        }
+    }
+}
